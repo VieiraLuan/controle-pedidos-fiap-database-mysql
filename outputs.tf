85c6cc0790abe0ace00a3bc3d34614f8644365a6
Luan Vieira <luandasilvavieira@hotmail.com>
2025-09-25 23:10:46 -0300

    Adding some parameters settings to be flexible and also store the tfstate in the correct container name

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,24 @@
 output "mysql_host" {
-  value = azurerm_mysql_flexible_server.mysql_server.fqdn
+  value       = azurerm_mysql_flexible_server.mysql_server.fqdn
+  description = "FQDN do servidor MySQL"
 }
 
 output "mysql_port" {
-  value = 3306
+  value       = 3306
+  description = "Porta do MySQL"
 }
 
 output "database_name" {
-  value = azurerm_mysql_flexible_server_database.db.name
+  value       = azurerm_mysql_flexible_server_database.db.name
+  description = "Nome do banco de dados criado"
+}
+
+output "mysql_connection_info" {
+  value = {
+    host = azurerm_mysql_flexible_server.mysql_server.fqdn
+    port = 3306
+    database = azurerm_mysql_flexible_server_database.db.name
+    user = var.admin_username
+  }
+  description = "Informações de conexão do MySQL"
 }
--- providers.tf
+++ providers.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name   = var.resource_group_name
+    storage_account_name  = var.storage_account_name
+    container_name        = var.container_name
+    key                   = "mysql.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
