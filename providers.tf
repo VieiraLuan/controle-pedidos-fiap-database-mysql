94f50beefdee2d0306e76b6a3defafb836bbccd5
Luan Vieira <luandasilvavieira@hotmail.com>
2025-09-28 16:38:38 -0300

    Updating providers

--- providers.tf
+++ providers.tf
@@ -12,7 +12,7 @@ terraform {
     resource_group_name   = var.resource_group_name
     storage_account_name  = var.storage_account_name
     container_name        = var.container_name
-    key                   = "mysql.terraform.tfstate"
+    key                   = "key=mysql.tfstate"
   }
 }
 
