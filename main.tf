8fe5360d49a55050c07211d934b5cde320a58b06
Luan Vieira <luandasilvavieira@hotmail.com>
2025-09-28 17:42:11 -0300

    Update main

--- main.tf
+++ main.tf
@@ -7,14 +7,24 @@ resource "azurerm_mysql_flexible_server" "mysql_server" {
   administrator_password = var.admin_password
   sku_name               = "B_Standard_B1ms"
 
-  storage_mb             = 5120
-  backup_retention_days  = 7
-  high_availability      = "Disabled"
+  storage {
+    size_gb = 5 # 5120 MB = 5 GB
+  }
+
+  backup {
+    backup_retention_days = 7
+  }
+
+  high_availability {
+    mode = "Disabled"
+  }
+
   public_network_access_enabled = true
 }
 
-resource "azurerm_mysql_flexible_server_database" "db" {
-  name                = var.database_name
-  resource_group_name = var.resource_group_name
-  server_name         = azurerm_mysql_flexible_server.mysql_server.name
+resource "azurerm_mysql_flexible_database" "db" {
+  name      = var.database_name
+  server_id = azurerm_mysql_flexible_server.mysql_server.id
+  charset   = "utf8"
+  collation = "utf8_general_ci"
 }
