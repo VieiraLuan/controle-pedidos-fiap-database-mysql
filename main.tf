501e34f12b071e616557d456d26b6c6c0aa24057
Luan Vieira <luandasilvavieira@hotmail.com>
2025-10-02 13:26:39 -0300

    update mysql version and sku

--- main.tf
+++ main.tf
@@ -2,19 +2,18 @@ resource "azurerm_mysql_server" "mysql_server" {
   name                = var.mysql_server_name
   location            = var.location
   resource_group_name = var.resource_group_name
+
   administrator_login          = var.admin_username
   administrator_login_password = var.admin_password
-  version = "8.0.21"
-  sku_name   = "B_Gen5_1"  
-  
-  storage {
-    size_gb = 20
-  }
-  
-  backup_retention_days = 7
-  geo_redundant_backup  = "Disabled"
-  auto_grow_enabled     = true
+  version                      = "8.0.21"
+  sku_name                     = "B_Gen5_1"
+
+  storage_mb                    = 20480
+  backup_retention_days         = 7
+  geo_redundant_backup_enabled  = false
+  auto_grow_enabled             = true
   public_network_access_enabled = true
+  ssl_enforcement_enabled       = true
 }
 
 resource "azurerm_mysql_database" "db" {
